04a67c25f8c4593a54cba4c5097ecb53b2ed1c9e
Michalis Georgiou <michalisgiorgiou@gmail.com>
2020-04-21 13:07:54 +0300

    My first web server

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
+import "net/http"
+
 // this is just a comment tha I updated for the third time
 func main() {
-	println("hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
 }
